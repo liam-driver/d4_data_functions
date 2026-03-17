d80aa09ae53fa029c864ff3252c89758dcce0ece
Liam Driver <109564670+liam-driver@users.noreply.github.com>
2026-03-17 16:19:48 +0000

    Added 90 day review section as well as refactor how the funnel dataframes are created and transformed

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ def main():
     with open("storage/config.json", "r") as config_json:
         clients = json.load(config_json)
     for client in clients:
-        # if client['report_due_date'] != datetime.today().strftime("%A"):
-        #     continue
+        if client['report_due_date'] != datetime.today().strftime("%A"):
+            continue
         print(client['name'])
         client = config_dates(client)
         try:
@@ -25,34 +25,32 @@ def main():
         except:
             log_error(f"{client['name']} Report Skipped: misconfigured 90 Day Plan")
             continue
-        if client['account_type'] == 'Lead Gen':
-            client['paid_data'] = get_funnel_data(client, 'paid_lead_gen')
-            client['llm_data'] = get_funnel_data(client, 'llm_lead_gen')
-            client['timeseries_data'] = get_funnel_data(client, 'time_series_lead_gen')
-            client['overall_data'] = get_funnel_data(client, 'overall_lead_gen')
-        if client['account_type'] == 'Ecommerce':
-            client['paid_data'] = get_funnel_data(client, 'paid_ecommerce')
-            client['llm_data'] = get_funnel_data(client, 'llm_ecommerce')
-            client['timeseries_data'] = get_funnel_data(client, 'time_series_ecommerce')
-            client['overall_data'] = get_funnel_data(client, 'overall_ecommerce')
-        
-        client['run_rate'] = get_run_rate(client)
+        try:
+            if client['account_type'] == 'Lead Gen':
+                client['paid_data'] = get_funnel_data(client, 'paid_lead_gen')
+                client['llm_data'] = get_funnel_data(client, 'llm_lead_gen')
+                client['timeseries_data'] = get_funnel_data(client, 'time_series_lead_gen')
+                client['overall_data'] = get_funnel_data(client, 'overall_lead_gen')
+            if client['account_type'] == 'Ecommerce':
+                client['paid_data'] = get_funnel_data(client, 'paid_ecommerce')
+                client['llm_data'] = get_funnel_data(client, 'llm_ecommerce')
+                client['timeseries_data'] = get_funnel_data(client, 'time_series_ecommerce')
+                client['overall_data'] = get_funnel_data(client, 'overall_ecommerce')
+            client['run_rate'] = get_run_rate(client)
+        except:
+            log_error(f"{client['name']} Report Skipped: misconfigured Paid Data") 
+            continue
 
-        # except:
-        #     log_error(f"{client['name']} Report Skipped: misconfigured Paid Data") 
-        #     continue
-        # try:
-        client['commentary'] = generate_commentary(client)
-        # except:
-        #     log_error(f"{client['name']} Report Skipped: misconfigured Commentary")
-        #     continue
-        with open('storage/commentary.json', 'w', encoding='utf-8') as f:
-            json.dump(client['commentary'], f, indent=4, default=str, ensure_ascii=False)
-        # try:
-        send_email(client)
-        # except:
-        #     log_error(f"{client['name']} Report Skipped: Error Sending Email")
-        #     continue
+        try:
+            client['commentary'] = generate_commentary(client)
+        except:
+            log_error(f"{client['name']} Report Skipped: misconfigured Commentary")
+            continue
+        try:
+            send_email(client)
+        except:
+            log_error(f"{client['name']} Report Skipped: Error Sending Email")
+            continue
     return 0
   
 main()
